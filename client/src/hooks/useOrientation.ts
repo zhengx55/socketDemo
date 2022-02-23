c6667a0ec89de8cb7f63f7c0b996abef3cfa403b
zhengx55 <williamzhxin@gmail.com>
2022-02-23 15:20:08 +0800

    mobie test

--- client/src/hooks/useOrientation.ts
+++ client/src/hooks/useOrientation.ts
@@ -5,19 +5,9 @@ import { isMobile } from "react-device-detect";
 function useOrientation(): (string | undefined)[] {
   // Initialize state with undefined width/height so server and client renders match
   const [orientation, setOrientation] = useState<string | undefined>(undefined);
-  // const [device, setDevice] = useState<string | undefined>(undefined);
   useEffect(() => {
     function handleResize(): void {
-      // if (
-      //   /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
-      //     ua
-      //   )
-      // ) {
-      //   setDevice("mobile");
-      // } else {
-      //   setDevice("desktop");
-      // }
-      if (window.screen.orientation.angle === 90) {
+      if (window.innerWidth > window.innerHeight) {
         setOrientation("landscape");
       } else {
         setOrientation("portrait");
