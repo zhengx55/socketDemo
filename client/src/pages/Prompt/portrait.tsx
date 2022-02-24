24f9a9423bea99f3d7471403e38956685661dbfc
zhengx55 <williamzhxin@gmail.com>
2022-02-24 14:23:42 +0800

    portrait

--- client/src/pages/Prompt/portrait.tsx
+++ client/src/pages/Prompt/portrait.tsx
@@ -15,7 +15,7 @@ const sizeIncrease = keyframes`
 
 const Background = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
