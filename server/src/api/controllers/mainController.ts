691b0773aee96311b3bcebfe7a58c84dc926cb1d
zhengx55 <williamzhxin@gmail.com>
2022-03-04 15:17:49 +0800

    update time

--- server/src/api/controllers/mainController.ts
+++ server/src/api/controllers/mainController.ts
@@ -93,7 +93,7 @@ export class MessageController {
             .emit("game_update_success", res.data);
         }
       }
-    }, 2000);
+    }, 20000);
     console.log("Socket disconnected:", socket.id);
   }
 }
