b81df0adfb0564d5bf546333d5c3ccb26f76816f
zhengx55 <williamzhxin@gmail.com>
2022-03-05 18:18:12 +0800

    build config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,4 +6,19 @@ export default defineConfig({
   plugins: [react()],
   root: "./",
   publicDir: "src/assets",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
