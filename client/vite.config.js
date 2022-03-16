f40a2b93278a6812446290a7034a5ea3a4b5c33f
zhengx55 <williamzhxin@gmail.com>
2022-03-16 15:12:57 +0800

    fix config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ["ie>=11"],
-      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
-    }),
-  ],
+  plugins: [react()],
   publicDir: "src/assets",
   base: "./",
 
